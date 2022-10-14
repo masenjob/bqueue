d46d3cfb1c8c7a4c7857763d95e29506b44721f1
masenjob <masenjo@gmail.com>
2022-10-14 19:36:34 -0300

    Update monitor.sh
    Freebsd compatibility

--- monitor.sh
+++ monitor.sh
@@ -3,12 +3,30 @@
 #
 # Displays status info of all queues
 #
-# 2020 Mauricio Asenjo
-# version 2.0rc4
+# 2022 Mauricio Asenjo
+# version 2.1rc1
 
 source ./bqueue_utils.sh
 
-format="%-15s %-10s %-10s %-10s %-10s %-10s %-10s"
+case "$OSTYPE" in
+linux*)
+	cpu_model=$(cat /proc/cpuinfo | grep -m 1 "model name")
+	cpu_count = $(cat /proc/cpuinfo | grep -c "model name")
+	;;
+
+freebsd*)
+	cpu_model=$(sysctl -n hw.model)
+	cpu_count=$(sysctl -n hw.ncpu)
+	;;
+	
+*)
+	cpu_model="NOT_SUPPORTED"
+	cpu_count="N/A"
+	;;
+esac
+
+
+format="%-15s %-10s %-10s %-10s %-10s %-10s %-10s\n"
 string="QUEUE STATE IN DELIVER PROCESSING FINISHED FAILED"
 
 while true
@@ -23,7 +41,7 @@ do
 	uptime
 	line=$(($line + 1))
 	tput cup $line 0
-	echo "CPU $(cat /proc/cpuinfo | grep -m 1 "model name") , count = $(cat /proc/cpuinfo | grep -c "model name")"
+	echo "CPU $cpu_model , count = $cpu_count"
 
 	line=$(($line + 2))
 	tput cup $line 0
